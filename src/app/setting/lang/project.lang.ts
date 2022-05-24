1ef5eebc9a3cf6807af3d1c70d3cd9797ee2efea
Aaron <radees.24.16@gmail.com>
2022-05-23 22:55:30 -0500

    add

--- src/app/setting/lang/project.lang.ts
+++ src/app/setting/lang/project.lang.ts
@@ -1,4 +1,5 @@
 import {ButtonLang} from "./button.lang";
+import {ContactConst} from "../constants/contact.const";
 
 export const ProjectLang = {
   data: [
@@ -17,7 +18,16 @@ export const ProjectLang = {
       url: 'https://joyful-sorbet-8bf8d5.netlify.app/',
       github: 'https://github.com/aaron-delgad/E-commerce-Frontends'
     },
+    {
+      title: 'Implementando una PWA (Progressive Web App)',
+      detail: `Se desarrollo en Angular version 13, generando el boton instalar para Desktop, Android e IOS,
+      además se configuró el service worker y se implementó la funcionalidad para enviar actualizaciones`,
+      color: 'bg--accent-light',
+      url: 'https://angular-pwa-f00d0.firebaseapp.com/product',
+      github: 'https://github.com/aaron-delgad/My_first_PWA'
+    },
   ],
   post: 'Para Desktop, Table y Mobile',
-  button: ButtonLang
+  button: ButtonLang,
+  gitHub: ContactConst.data[1],
 }
