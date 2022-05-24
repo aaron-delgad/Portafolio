b284c585e090ef00a2e9ee583a46e9ee548bc1a5
Aaron <radees.24.16@gmail.com>
2022-05-24 11:41:10 -0500

    projectos

--- src/app/setting/lang/project.lang.ts
+++ src/app/setting/lang/project.lang.ts
@@ -4,27 +4,57 @@ import {ContactConst} from "../constants/contact.const";
 export const ProjectLang = {
   data: [
     {
-      title: 'Proyecto desarrollado aplicando JavaScript, HTML y CSS puro.',
+      title: 'Proyecto desarrollado en la empresa Rumilab',
+      detail: `Permite realizar transacciones interbancarias y pago de servicios cobrando una
+      comisión muy baja, solo debes crear tu cuenta y listo.`,
+      color: 'bg--primary-light',
+      url: 'https://ahoramas.pe/#/web/inicio',
+      github: ''
+    },
+    {
+      title: 'Proyecto desarrollado en la empresa FitCoders',
+      detail: `Puedes consultar DNI y RUC, ademas brinda servicios adicionales, si deseas algun servicio
+      puedes llenar el formulario de contacto.`,
+      color: 'bg--accent',
+      url: 'https://perudevs.com/#/web/inicio',
+      github: ''
+    },
+    {
+      title: 'Aplicativo movil desarrollado en IONIC, empresa Rumilab',
+      detail: `Disponible para Android, en Google Play.`,
+      color: 'bg--primary-light',
+      url: 'https://play.google.com/store/apps/details?id=rumilab.com.ahora.app',
+      github: ''
+    },
+    {
+      title: 'Aplicativo movil desarrollado en IONIC, empresa Rumilab',
+      detail: `Disponible para IOS, en App Store.`,
+      color: 'bg--accent',
+      url: 'https://apps.apple.com/pe/app/ahora-m%C3%A1s/id1610943754',
+      github: ''
+    },
+    {
+      title: 'Implementando una PWA (Progressive Web App)',
+      detail: `Se desarrollo en Angular version 13, generando el boton instalar para Desktop, Android e IOS,
+      además se configuró el service worker y se implementó la funcionalidad para enviar actualizaciones`,
+      color: 'bg--primary-light',
+      url: 'https://angular-pwa-f00d0.firebaseapp.com/product',
+      github: 'https://github.com/aaron-delgad/My_first_PWA'
+    },
+    {
+      title: 'Desarrollado aplicando JavaScript, HTML y CSS puro.',
       detail: 'Calcula el área y perímetro de un Triángulo, Círculo y Cuadrado',
       color: 'bg--accent',
       url: 'https://aaron-delgad.github.io/Curso-Practico-JavaScript/',
       github: 'https://github.com/aaron-delgad/Curso-Practico-JavaScript'
     },
     {
-      title: 'Proyecto desarrollado implementando HTML y CSS puro',
+      title: 'Proyecto desarrollado aplicando HTML y CSS puro',
       detail: `Solo se ha desarrollado las interfaces gráficas de las diferentes páginas
       que involucra una E-Commerce`,
       color: 'bg--primary-light',
       url: 'https://joyful-sorbet-8bf8d5.netlify.app/',
       github: 'https://github.com/aaron-delgad/E-commerce-Frontends'
-    },
-    {
-      title: 'Implementando una PWA (Progressive Web App)',
-      detail: `Se desarrollo en Angular version 13, generando el boton instalar para Desktop, Android e IOS,
-      además se configuró el service worker y se implementó la funcionalidad para enviar actualizaciones`,
-      color: 'bg--accent-light',
-      url: 'https://angular-pwa-f00d0.firebaseapp.com/product',
-      github: 'https://github.com/aaron-delgad/My_first_PWA'
     },
   ],
   post: 'Para Desktop, Table y Mobile',
